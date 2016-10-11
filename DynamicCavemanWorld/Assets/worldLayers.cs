1473f36f8d880b025ff0ac8c6059ecf3fc2f811d
William Huebler <hueblerw@gmail.com>
2016-10-11 09:03:12 -0500

    Created a blank class file for layers superclass

---
+++ DynamicCavemanWorld/Assets/worldLayers.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class worldLayers : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	    // Unclear if needed
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    // Unclear if needed
+	}
+}
