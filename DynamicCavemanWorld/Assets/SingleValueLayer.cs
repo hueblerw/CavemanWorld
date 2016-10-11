29d4f9a2286b3fe72ef6912b1cefb5f9861a2a2e
William Huebler <hueblerw@gmail.com>
2016-10-11 09:25:34 -0500

    Implementation of the superclass appears to have failed and is definitely not worth it, transfered all contents to the subclass and removed the superclass

---
+++ DynamicCavemanWorld/Assets/SingleValueLayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SingleValueLayer
+{
+
+    public string layerName;
+    private string layerType;
+    public const int WORLDX = 50;
+    public const int WORLDZ = 50;
+
+    public string getType()
+    {
+        return layerType;
+    }
+
+}
