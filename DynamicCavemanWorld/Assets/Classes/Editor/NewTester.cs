d848c05e8edf511b976a8c105c6332c99dd17f94
William Huebler <hueblerw@gmail.com>
2016-10-11 10:08:18 -0500

    Create a testfile for the SVLayer class, all tests appear to pass

---
+++ DynamicCavemanWorld/Assets/Classes/Editor/NewTester.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEditor;
+using NUnit.Framework;
+
+public class NewTester {
+
+	[Test]
+	public void SVLayerTest()
+	{
+        // Test all the SingleLayer Values can be initialized
+        SingleValueLayer elevation = new SingleValueLayer("Elevation", "Semi-static");
+
+        Assert.AreEqual("Elevation", elevation.layerName);
+        Assert.AreEqual("Semi-static", elevation.getType());
+        Assert.AreEqual(50, SingleValueLayer.WORLDX);
+        Assert.AreEqual(50, SingleValueLayer.WORLDZ);
+    }
+}
