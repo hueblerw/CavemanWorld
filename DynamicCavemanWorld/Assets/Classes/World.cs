d6d02aafe23e419c249a342f8bd73ef9ab6ea27e
William Huebler <hueblerw@gmail.com>
2016-10-11 15:33:57 -0500

    Create a World Object and its basic constructor equation, only dealing with elevation and temperature

---
+++ DynamicCavemanWorld/Assets/Classes/World.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class World : MonoBehaviour {
+
+    // Layers
+    SingleValueLayer elevation = new SingleValueLayer("Elevation", "Semi-static", 1);
+    SingleValueLayer highTemp = new SingleValueLayer("HighTemp", "Semi-static", 0);
+    SingleValueLayer lowTemp = new SingleValueLayer("LowTemp", "Semi-static", 0);
+    SingleValueLayer tempMidpt = new SingleValueLayer("TempMidpoint", "Semi-static", 1);
+    SingleValueLayer variance = new SingleValueLayer("Variance", "Semi-static", 1);
+    EquationLayer temperatureEquations = new EquationLayer("TemperatureEquations", "Semi-static", 6, 1);
+
+    // Constructor
+    public World()
+    {
+        string filePathPrefix = @"C:\Users\William\Documents\World Generator Maps\CavemanWorld\DynamicCavemanWorld\Assets\Resources\CSV\";
+        // Elevation info
+        elevation.readCSVFile(filePathPrefix + "ElevationNiceMapA.csv");
+        // Temperature info
+        highTemp.readCSVFile(filePathPrefix + "HighTempNiceMapA.csv");
+        lowTemp.readCSVFile(filePathPrefix + "LowTempNiceMapA.csv");
+        tempMidpt.readCSVFile(filePathPrefix + "TempMidptNiceMapA.csv");
+        variance.readCSVFile(filePathPrefix + "VarianceNiceMapA.csv");
+        temperatureEquations.equations = new TempEquation();
+        // Rainfall info
+
+    }
+    
+    // Other methods!
+}
