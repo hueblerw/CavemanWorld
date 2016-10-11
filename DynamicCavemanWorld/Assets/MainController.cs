88ed1a4dbbbe56d83629a8d2981bd70a632024a1
William Huebler <hueblerw@gmail.com>
2016-10-11 09:42:37 -0500

    Create a temporary test controller linked to a flat plane object

---
+++ DynamicCavemanWorld/Assets/MainController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SingleValueLayer Elevation = new SingleValueLayer("Elevation", "Semi-static");
+        Debug.Log("Hello World!");
+        Debug.Log(Elevation);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
