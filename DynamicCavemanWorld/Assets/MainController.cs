6c3a0aa83f63ea17d15c840096dcd25e75486a09
William Huebler <hueblerw@gmail.com>
2016-10-11 10:08:59 -0500

    Remove the test debuggers from the MainController

--- DynamicCavemanWorld/Assets/MainController.cs
+++ DynamicCavemanWorld/Assets/MainController.cs
@@ -7,10 +7,6 @@ public class MainController : MonoBehaviour {
 	void Start () {
         SingleValueLayer elevation = new SingleValueLayer("Elevation", "Semi-static");
         Debug.Log("Hello World!");
-        Debug.Log(elevation.layerName);
-        Debug.Log(elevation.getType());
-        Debug.Log(SingleValueLayer.WORLDX);
-        Debug.Log(SingleValueLayer.WORLDZ);
     }
 	
 	// Update is called once per frame
