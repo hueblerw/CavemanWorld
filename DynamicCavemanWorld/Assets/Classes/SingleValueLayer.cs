7edbcfcb31d0c0ad2d502628f61f1f23280a5b81
William Huebler <hueblerw@gmail.com>
2016-10-11 10:24:45 -0500

    Add the rounded variable its getter, and define the type of array in the SVLayer class

--- DynamicCavemanWorld/Assets/Classes/SingleValueLayer.cs
+++ DynamicCavemanWorld/Assets/Classes/SingleValueLayer.cs
@@ -4,23 +4,35 @@ using System.Collections;
 public class SingleValueLayer
 {
 
+    // Variables
     public string layerName;
     private string layerType;
+    private int rounded;
     public static int WORLDX = 50;
     public static int WORLDZ = 50;
-    public float[][] worldArray;
+    public decimal[,] worldArray = new decimal[WORLDX, WORLDZ];
 
-
-    public SingleValueLayer(string name, string type)
+    // Constructor
+    public SingleValueLayer(string name, string type, int roundTo)
     {
         this.layerName = name;
         this.layerType = type;
+        this.rounded = roundTo;
     }
 
+    // Layer initialization Method
 
+
+
+    // Getter methods
     public string getType()
     {
         return layerType;
+    }
+
+    public int getRounding()
+    {
+        return rounded;
     }
 
 }
