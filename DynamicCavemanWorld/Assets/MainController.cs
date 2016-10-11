29db329076074304ce5e639cb2fb8b6949f50303
William Huebler <hueblerw@gmail.com>
2016-10-11 10:30:11 -0500

    Main controller now inputs the rounded variable

--- DynamicCavemanWorld/Assets/MainController.cs
+++ DynamicCavemanWorld/Assets/MainController.cs
@@ -5,7 +5,7 @@ public class MainController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        SingleValueLayer elevation = new SingleValueLayer("Elevation", "Semi-static");
+        SingleValueLayer elevation = new SingleValueLayer("Elevation", "Semi-static", 1);
         Debug.Log("Hello World!");
     }
 	
