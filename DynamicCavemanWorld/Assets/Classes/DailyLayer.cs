9dad6e71acb04adc7c2629f5ba572e721d2f03c0
William Huebler <hueblerw@gmail.com>
2016-10-11 10:21:31 -0500

    Create a Daily Layer class

---
+++ DynamicCavemanWorld/Assets/Classes/DailyLayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DailyLayer {
+
+    // Variables
+    public string layerName;
+    private string layerType;
+    private int rounded;
+    public static int WORLDX = 50;
+    public static int WORLDZ = 50;
+    public decimal[,,] worldArray = new decimal[120, WORLDX, WORLDZ];
+
+    // Constructor
+    public DailyLayer(string name, string type, int roundTo)
+    {
+        this.layerName = name;
+        this.layerType = type;
+        this.rounded = roundTo;
+    }
+
+    // World Array Initializer
+
+    // Getters
+    public string getType()
+    {
+        return layerType;
+    }
+
+    public int getRounding()
+    {
+        return rounded;
+    }
+
+}
