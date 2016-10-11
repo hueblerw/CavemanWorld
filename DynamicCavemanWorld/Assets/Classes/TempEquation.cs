e2f1b7371eb69b3212001439856f532ffc0b4660
William Huebler <hueblerw@gmail.com>
2016-10-11 15:19:24 -0500

    Create the empty template for the temperature equation object

---
+++ DynamicCavemanWorld/Assets/Classes/TempEquation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TempEquation {
+
+    // Variables
+    private decimal A;
+    private decimal B;
+    private decimal C;
+    private decimal D;
+    private decimal E;
+    private decimal F;
+    private decimal G;
+
+    // Constructor: takes in the input from the variable semi-static temperature references
+    public TempEquation()
+    {
+        // Lotsa fun math here
+    }
+
+    // Return an array of random temperatures for the year
+    public decimal[] newYearsTemps()
+    {
+        // Lotsa fun math here!
+    }
+
+}
