49f1e4e9cd256015a66bd15c3d31dbc226ddea5c
William Huebler <hueblerw@gmail.com>
2016-10-11 10:29:48 -0500

    Create new test for the Daily Layer, also create a test for the newly added rounded variable

--- DynamicCavemanWorld/Assets/Classes/Editor/NewTester.cs
+++ DynamicCavemanWorld/Assets/Classes/Editor/NewTester.cs
@@ -8,11 +8,25 @@ public class NewTester {
 	public void SVLayerTest()
 	{
         // Test all the SingleLayer Values can be initialized
-        SingleValueLayer elevation = new SingleValueLayer("Elevation", "Semi-static");
+        SingleValueLayer elevation = new SingleValueLayer("Elevation", "Semi-static", 1);
 
         Assert.AreEqual("Elevation", elevation.layerName);
         Assert.AreEqual("Semi-static", elevation.getType());
+        Assert.AreEqual(1, elevation.getRounding());
         Assert.AreEqual(50, SingleValueLayer.WORLDX);
         Assert.AreEqual(50, SingleValueLayer.WORLDZ);
+    }
+
+    [Test]
+    public void DailyLayerTest()
+    {
+        // Test all the SingleLayer Values can be initialized
+        DailyLayer rainfall = new DailyLayer("Rainfall", "Daily", 1);
+
+        Assert.AreEqual("Elevation", rainfall.layerName);
+        Assert.AreEqual("Daily", rainfall.getType());
+        Assert.AreEqual(1, rainfall.getRounding());
+        Assert.AreEqual(50, DailyLayer.WORLDX);
+        Assert.AreEqual(50, DailyLayer.WORLDZ);
     }
 }
