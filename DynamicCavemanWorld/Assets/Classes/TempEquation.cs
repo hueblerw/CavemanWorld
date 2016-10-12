d2ee96ceb4a994a452fd9b9c23913aa12b8d0e4c
William Huebler <hueblerw@gmail.com>
2016-10-12 12:41:50 -0500

    Finish the matrix code up to the point where we use the solutions

--- DynamicCavemanWorld/Assets/Classes/TempEquation.cs
+++ DynamicCavemanWorld/Assets/Classes/TempEquation.cs
@@ -27,18 +27,26 @@ public class TempEquation {
 
         // Then create a matrix using the midpt input
         float fallMidpt = 120 - Midpt;
+        double midptRatio = Midpt / 120;
+        double fallMidptRatio = fallMidpt / 120;
+        double[,] solutionArray = { { 0.0 },
+                                    { 0.0 },
+                                    { 0.0 },
+                                    { 0.25 - midptRatio },
+                                    { 0.75 - fallMidptRatio } };
         double[,] systemArray = {{ 1.0, 1.0, 1.0, 1.0, 1.0 },
                         { 0.5, 1/3, 0.25, 0.2, 1/6 },
                         { Math.Pow(.5, 6) / 6, Math.Pow(.5, 5) / 5, Math.Pow(.5, 4) / 4, Math.Pow(.5, 3) / 3, Math.Pow(.5, 2) / 2 },
-                        { Math.Pow(Midpt / 120, 6) / 6, Math.Pow(Midpt / 120, 5) / 5, Math.Pow(Midpt / 120, 4) / 4, Math.Pow(Midpt / 120, 3) / 3, Math.Pow(Midpt / 120, 2) / 2 },
-                        { Math.Pow(fallMidpt / 120, 6) / 6, Math.Pow(fallMidpt / 120, 5) / 5, Math.Pow(fallMidpt / 120, 4) / 4, Math.Pow(fallMidpt / 120, 3) / 3, Math.Pow(fallMidpt / 120, 2) / 2 }};
+                        { Math.Pow(midptRatio, 6) / 6, Math.Pow(midptRatio, 5) / 5, Math.Pow(midptRatio, 4) / 4, Math.Pow(midptRatio, 3) / 3, Math.Pow(midptRatio, 2) / 2 },
+                        { Math.Pow(fallMidptRatio, 6) / 6, Math.Pow(fallMidptRatio, 5) / 5, Math.Pow(fallMidptRatio, 4) / 4, Math.Pow(fallMidptRatio, 3) / 3, Math.Pow(fallMidptRatio, 2) / 2 }};
         Matrix<double> A = Matrix<double>.Build.DenseOfArray(systemArray);
+        Matrix<double> B = Matrix<double>.Build.DenseOfArray(solutionArray);
         // Then solve matrix A's inverse
-
+        Matrix<double> inverse = A.Inverse();
         // Then multiply matrix A-1 x B
-
+        Matrix<double> solutions = inverse * B;
         // Extract the answers for A-E
-
+        Debug.Log(solutions);
     }
 
     // Return an array of random temperatures for the year
