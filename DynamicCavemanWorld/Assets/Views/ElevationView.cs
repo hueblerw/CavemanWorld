45c1047b92f67deb545bff89d6fea4f2388adcb0
William Huebler <hueblerw@gmail.com>
2016-10-14 10:21:13 -0500

    Add triangles so now we have blank squares!

--- DynamicCavemanWorld/Assets/Views/ElevationView.cs
+++ DynamicCavemanWorld/Assets/Views/ElevationView.cs
@@ -14,11 +14,12 @@ public class ElevationView : MonoBehaviour {
         
         // Convert to mesh data
         Vector3[] vertices = new Vector3[numVertices];
-        int[] triangles = new int[2 * numOfTilesZ * 3];
+        int[] triangles = new int[2 * numOfTiles * 3];
         Vector3[] normals = new Vector3[numVertices];
 
         // Create the vertices and the normals generically
         int x, z;
+        int squareIndex, triOffset;
         for (z = 0; z < numOfTilesZ + 1; z++)
         {
             for (x = 0; x < numOfTilesX + 1; x++)
@@ -28,9 +29,26 @@ public class ElevationView : MonoBehaviour {
                 // uv[z * (numOfTilesX + 1) + x] = new Vector2((float) x / (numOfTilesX + 1), (float) z / (numOfTilesZ + 1));
             }
         }
+        // Create the triangle generically
+        for (z = 0; z < numOfTilesZ; z++)
+        {
+            for (x = 0; x < numOfTilesX; x++)
+            {
+                squareIndex = z * numOfTilesX + x;
+                triOffset = squareIndex * 6;
+                // first triangle
+                triangles[triOffset] = z * (numOfTilesX + 1) + x + 0;
+                triangles[triOffset + 2] = z * (numOfTilesX + 1) + x + (numOfTilesX + 1) + 1;
+                triangles[triOffset + 1] = z * (numOfTilesX + 1) + x + (numOfTilesX + 1) + 0; ;
+                // second triangle
+                triangles[triOffset + 3] = z * (numOfTilesX + 1) + x + 0;
+                triangles[triOffset + 5] = z * (numOfTilesX + 1) + x + 1;
+                triangles[triOffset + 4] = z * (numOfTilesX + 1) + x + (numOfTilesX + 1) + 1; ;
+            }
+        }
 
-        // Create a new mesh and populate it with the data from the elevation layer
-        Mesh world = new Mesh();
+                // Create a new mesh and populate it with the data from the elevation layer
+                Mesh world = new Mesh();
         world.vertices = vertices;
         world.triangles = triangles;
         world.normals = normals;
