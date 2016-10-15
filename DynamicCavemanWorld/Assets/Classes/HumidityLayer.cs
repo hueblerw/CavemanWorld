165491847a0bbe5efe10606375a7df22db6c574f
William Huebler <hueblerw@gmail.com>
2016-10-15 14:01:42 -0500

    Create a separate humidity layer class for rainfall generation

---
+++ DynamicCavemanWorld/Assets/Classes/HumidityLayer.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using System;
+
+public class HumidityLayer
+{
+
+    // Variables
+    public string layerName;
+    private int rounded;
+    public static int WORLDX;
+    public static int WORLDZ;
+    public int[] timeIntervals;
+    public float[,] worldArray = new float[WORLDX, WORLDZ];
+    // public float[,,] worldArray = new float[timeIntervals.Length, WORLDX, WORLDZ];
+        // THIS IS WHAT THE END GOAL ARRAY LOOKS LIKE
+        // FOR NOW WE ARE JUST USING A SINGLE NUMBER FOR THE WHOLE YEAR FOR TESTING PURPOSES
+
+    // Constructor
+    public HumidityLayer(string name, int roundTo)
+    {
+        this.layerName = name;
+        this.rounded = roundTo;
+    }
+
+    // Layer initialization Method
+    public void readCSVFile(string filePath)
+    {
+
+        StreamReader sr = new StreamReader(filePath);
+        float[,] data = new float[WORLDX, WORLDZ];
+        int Row = 0;
+        while (!sr.EndOfStream)
+        {
+            string[] Line = sr.ReadLine().Split(',');
+            for (int i = 0; i < Line.Length; i++)
+            {
+                data[i, Row] = (float)Convert.ToDouble(Line[i]);
+            }
+            Row++;
+        }
+        this.worldArray = data;
+
+    }
+
+    // Getter methods
+    public int getRounding()
+    {
+        return rounded;
+    }
+
+}
