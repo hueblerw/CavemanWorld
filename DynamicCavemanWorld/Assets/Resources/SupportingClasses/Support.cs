d14cf74920fd7f9a7cb664615933769741583217
William Huebler <hueblerw@gmail.com>
2016-10-19 11:37:07 -0500

    Fixed so that center around vertex is different from center around cells

--- DynamicCavemanWorld/Assets/Resources/SupportingClasses/Support.cs
+++ DynamicCavemanWorld/Assets/Resources/SupportingClasses/Support.cs
@@ -7,21 +7,21 @@ public class Support {
     public static float[] CellsAroundVertex(int x, int z, int WorldX, int WorldZ, float[,] array)
     {
         float[] cells;
-        List<float> cellList = Support.getAround(false, x, z, WorldX, WorldZ, array);
+        List<float> cellList = Support.getAround(x, z, WorldX, WorldZ, array);
         cells = cellList.ToArray();
         return cells;
     }
 
-    public static float[] CellsAllAroundVertex(int x, int z, int WorldX, int WorldZ, float[,] array)
+    public static float[] CellsAllAroundC(int x, int z, int WorldX, int WorldZ, float[,] array)
     {
         float[] cells;
-        List<float> cellList = Support.getAround(true, x, z, WorldX, WorldZ, array); ;
+        List<float> cellList = Support.getAroundCenter(true, x, z, WorldX, WorldZ, array); ;
         cells = cellList.ToArray();
         return cells;
     }
 
     // Private methods
-    private static List<float> getAround(bool diagonals, int x, int z, int WorldX, int WorldZ, float[,] array)
+    private static List<float> getAround(int x, int z, int WorldX, int WorldZ, float[,] array)
     {
         List<float> cellList = new List<float>();
         // Add the four caridnal values if legal
@@ -40,6 +40,30 @@ public class Support {
         if (x < WorldX && z > 0)
         {
             cellList.Add(array[x, z - 1]);
+        }
+
+        return cellList;
+    }
+
+    private static List<float> getAroundCenter(bool diagonals, int x, int z, int WorldX, int WorldZ, float[,] array)
+    {
+        List<float> cellList = new List<float>();
+        // Add the four caridnal values if legal
+        if (x > 0)
+        {
+            cellList.Add(array[x - 1, z]);
+        }
+        if (x < WorldX)
+        {
+            cellList.Add(array[x + 1, z]);
+        }
+        if (z > 0)
+        {
+            cellList.Add(array[x, z - 1]);
+        }
+        if (z < WorldZ)
+        {
+            cellList.Add(array[x, z + 1]);
         }
         // Add the diagonals if required and legal
         if (diagonals)
