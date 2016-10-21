06f933c9cc493562894916396f13b7933d6cf5f8
William Huebler <hueblerw@gmail.com>
2016-10-21 10:02:40 -0500

    Add more to the update method and pseudocode out what is needed

--- DynamicCavemanWorld/Assets/Controllers/TileMouseOver.cs
+++ DynamicCavemanWorld/Assets/Controllers/TileMouseOver.cs
@@ -1,20 +1,44 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class TileMouseOver : MonoBehaviour {
-	
+
+    public Text TileInfo;
+    public Text TimeInfo;
+    public int year;
+    public int day;
 	// Update is called once per frame
+    void Start()
+    {
+        year = 1;
+        day = 1;
+        TileInfo.text = "Square Info:";
+        TimeInfo.text = "Year: " + year + "        Day: " + day;
+    }
+
 	void Update () {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hitInfo;
 
         if(GetComponent<Collider>().Raycast(ray, out hitInfo, Mathf.Infinity))
         {
-            GetComponent<Renderer>().material.color = Color.red;
+            UpdateTileInfo();
         }
         else
         {
-            GetComponent<Renderer>().material.color = Color.green;
+            // GetComponent<Renderer>().material.color = Color.green;
         }
 	}
+
+    void UpdateTileInfo()
+    {
+        // Somehow grab the information from the World object to display here.
+            // elevation
+            // tempToday
+            // rainToday
+            // surfaceWaterToday
+            // hillPer
+            // oceanPer
+    }
 }
