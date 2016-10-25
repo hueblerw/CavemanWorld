79654f8635697f3b89cbab6500a906140428987a
William Huebler <hueblerw@gmail.com>
2016-10-25 11:51:41 -0500

    Create timing tests to test length of time for world creation and for generating a new year

---
+++ DynamicCavemanWorld/Assets/Classes/Editor/TimingTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Diagnostics;
+
+public class TimingTests {
+
+	[Test]
+	public void WorldCreationTime()
+	{
+        World testWorld = new World(50, 50);
+	}
+
+    [Test]
+    public void NewYearTime()
+    {
+        World testWorld = new World(50, 50);
+        Stopwatch watch = new Stopwatch();
+        watch.Start();
+            testWorld.NewYear();
+        watch.Stop();
+        Debug.Print(watch.Elapsed.TotalSeconds.ToString());
+    }
+}
