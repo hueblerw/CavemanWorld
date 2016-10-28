845ae2b1f4756b7b0fdc2b4d5d931360c474787a
William Huebler <hueblerw@gmail.com>
2016-10-28 12:03:34 -0500

    Create a basic Habitat class

---
+++ DynamicCavemanWorld/Assets/Classes/Habitat.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Habitat {
+
+    // Variables
+    public string type;
+
+    // Constructor
+    public Habitat()
+    {
+
+    }
+
+    // Habitat Yearly Update Method
+
+}
