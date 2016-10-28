8743a75c8b2ea943ec7f9df2c8a615d18c8d2981
William Huebler <hueblerw@gmail.com>
2016-10-28 12:00:58 -0500

    Create new basic Habitat Layer Class

---
+++ DynamicCavemanWorld/Assets/Classes/HabitatLayer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HabitatLayer {
+
+    // Variables
+    public string layerName;
+    private string layerType;
+    public static int WORLDX;
+    public static int WORLDZ;
+    public float[,] worldArray;
+
+    // Constructor
+    public HabitatLayer()
+    {
+        layerName = "Habitat Layer";
+        layerType = "Yearly";
+    }
+
+    // Getter methods
+    public string getType()
+    {
+        return layerType;
+    }
+
+}
