6f66f7ac1e55de2df6bfe111aa834578b9cf08b2
William Huebler <hueblerw@gmail.com>
2016-10-28 12:30:16 -0500

    Begin writing the Update the habitat method

--- DynamicCavemanWorld/Assets/Classes/Habitat.cs
+++ DynamicCavemanWorld/Assets/Classes/Habitat.cs
@@ -3,8 +3,12 @@ using System.Collections;
 
 public class Habitat {
 
+    // Constant
+    private float EnvironmentalShiftFactor = .01f;
+
     // Variables
-    public string type;
+    public string dominantType;
+    public float[] typePercents;
 
     // Constructor
     public Habitat()
@@ -13,5 +17,49 @@ public class Habitat {
     }
 
     // Habitat Yearly Update Method
+    public void UpdateHabitatYear(int hotDays, int coldDays, float rain, int snowCovered)
+    {
 
+        // Wetness determination statement
+        string wetness;
+        if(rain < 20f)
+        {
+            wetness = "dry";
+        }
+        else
+        {
+            if (rain < 40f)
+            {
+                wetness = "moderate";
+            }
+            else
+            {
+                if (rain < 60f)
+                {
+                    wetness = "wet";
+                }
+                else
+                {
+                    wetness = "very wet";
+                }
+            }
+        }
+
+        // Temperature ifs
+        string temperature = "temperate";
+        if (hotDays > 40 && coldDays < 10)
+        {
+            temperature = "tropical";
+        }
+        else {
+            if (hotDays < 10 && coldDays > 40)
+            {
+                temperature = "artic";
+            }
+        }
+
+        // Get the favored habitat
+
+        
+    }
 }
