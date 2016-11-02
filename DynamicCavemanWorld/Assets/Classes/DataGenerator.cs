937becd4ecb0f28543f1a68e8da5f7c735539061
William Huebler <hueblerw@gmail.com>
2016-11-02 10:48:57 -0500

    Created methods that create tempmidpt, variance, and avghumidity layers

--- DynamicCavemanWorld/Assets/Classes/DataGenerator.cs
+++ DynamicCavemanWorld/Assets/Classes/DataGenerator.cs
@@ -10,14 +10,52 @@ public class DataGenerator {
 
     // Static methods for creating layers based on inputs:
     // ***************************************************
-    // Standard Layer - float [,]
+    // Standard Layer - float [,] - note this works for TempMidpt, Variance, and AvgHumidity layer generation
     public static float[,] CreateStandardFloatLayer(int WORLDX, int WORLDZ, double minValue, double maxValue, double distMult)
     {
         float[,] layer = new float[WORLDX, WORLDZ];
-        Random randy = new Random();
+        System.Random randy = new System.Random();
+        int sign = 1;
+        double average;
 
         // First do the upper left square
-        layer[0, 0] = randy.NextDouble(minValue, maxValue);
+        layer[0, 0] = (float) (randy.NextDouble() * (maxValue - minValue) + minValue);
+        // Then do the top row based on the square to its left
+        for (int x = 1; x < WORLDX; x++)
+        {
+            sign = GenerateRandomSign(randy);
+            layer[x, 0] = (float) (layer[x - 1, 0] + sign * randy.NextDouble() * distMult);
+        }
+        // Then do the left column based on the square above it
+        for (int z = 1; z < WORLDZ; z++)
+        {
+            sign = GenerateRandomSign(randy);
+            layer[0, z] = (float)(layer[z - 1, 0] + sign * randy.NextDouble() * distMult);
+        }
+        // Then do every other cell in order
+        for (int z = 1; z < WORLDX; z++)
+        {
+            for (int x = 1; x < WORLDX; x++)
+            {
+                sign = GenerateRandomSign(randy);
+                average = (layer[z - 1, x] + layer[z - 1, x - 1] + layer[z, x - 1]) / 3.0;
+                layer[0, z] = (float)(average + sign * randy.NextDouble() * distMult);
+            }
+        }
+
+        return layer;
+    }
+
+
+    // Private supporting methods
+    private static int GenerateRandomSign(System.Random randy)
+    {
+        int num = randy.Next(0, 2);
+        if (num == 0)
+        {
+            num = -1;
+        }
+        return num;
     }
 
 }
