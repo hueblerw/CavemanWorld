7ca19710584371fd0f3a6225352fb06f3419e37e
William Huebler <hueblerw@gmail.com>
2016-11-02 11:16:25 -0500

    Create methods for creating the high and low temperature layers

--- DynamicCavemanWorld/Assets/Classes/DataGenerator.cs
+++ DynamicCavemanWorld/Assets/Classes/DataGenerator.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class DataGenerator {
 
@@ -15,7 +16,7 @@ public class DataGenerator {
     {
         float[,] layer = new float[WORLDX, WORLDZ];
         System.Random randy = new System.Random();
-        int sign = 1;
+        int sign;
         double average;
 
         // First do the upper left square
@@ -24,13 +25,13 @@ public class DataGenerator {
         for (int x = 1; x < WORLDX; x++)
         {
             sign = GenerateRandomSign(randy);
-            layer[x, 0] = (float) (layer[x - 1, 0] + sign * randy.NextDouble() * distMult);
+            layer[x, 0] = (float) Math.Max(Math.Min((layer[x - 1, 0] + sign * randy.NextDouble() * distMult), maxValue), minValue);
         }
         // Then do the left column based on the square above it
         for (int z = 1; z < WORLDZ; z++)
         {
             sign = GenerateRandomSign(randy);
-            layer[0, z] = (float)(layer[z - 1, 0] + sign * randy.NextDouble() * distMult);
+            layer[0, z] = (float) Math.Max(Math.Min((layer[z - 1, 0] + sign * randy.NextDouble() * distMult), maxValue), minValue);
         }
         // Then do every other cell in order
         for (int z = 1; z < WORLDX; z++)
@@ -39,7 +40,7 @@ public class DataGenerator {
             {
                 sign = GenerateRandomSign(randy);
                 average = (layer[z - 1, x] + layer[z - 1, x - 1] + layer[z, x - 1]) / 3.0;
-                layer[0, z] = (float)(average + sign * randy.NextDouble() * distMult);
+                layer[0, z] = (float) Math.Max(Math.Min((average + sign * randy.NextDouble() * distMult), maxValue), minValue);
             }
         }
 
@@ -47,7 +48,90 @@ public class DataGenerator {
     }
 
 
+    // Temperature Layers - linked int [,] - note this works for AvgHigh and LowTemp
+    public static int[][,] CreateTemperatureLayers(int WORLDX, int WORLDZ, int minValue, int maxValue, int distMult)
+    {
+        int[][,] layers = new int[2][,];
+        layers[0] = new int[WORLDX, WORLDZ];
+        layers[1] = new int[WORLDX, WORLDZ];
+        System.Random randy = new System.Random();
+        int sign;
+        int average;
+
+        // Create the hightemperature layer
+        layers[0] = CreateStandardIntLayer(WORLDX, WORLDZ, 30, 110, 4);
+
+        // Then create the linked LowTemperature layer
+        minValue = -20;
+        // First do the upper left square
+        layers[1][0, 0] = randy.Next(minValue, layers[0][0, 0] - 15);
+        // Then do the top row based on the square to its left
+        for (int x = 1; x < WORLDX; x++)
+        {
+            sign = GenerateRandomSign(randy);
+            maxValue = layers[0][x, 0] - 15;
+            layers[1][x, 0] = Math.Max(Math.Min((layers[1][x - 1, 0] + sign * randy.Next(0, distMult + 1)), maxValue), minValue);
+        }
+        // Then do the left column based on the square above it
+        for (int z = 1; z < WORLDZ; z++)
+        {
+            sign = GenerateRandomSign(randy);
+            maxValue = layers[0][0, z] - 15;
+            layers[1][0, z] = Math.Max(Math.Min((layers[1][z - 1, 0] + sign * randy.Next(0, distMult + 1)), maxValue), minValue);
+        }
+        // Then do every other cell in order
+        for (int z = 1; z < WORLDX; z++)
+        {
+            for (int x = 1; x < WORLDX; x++)
+            {
+                sign = GenerateRandomSign(randy);
+                maxValue = layers[0][x, z] - 15;
+                average = (int)Math.Round((layers[1][z - 1, x] + layers[1][z - 1, x - 1] + layers[1][z, x - 1]) / 3.0, 0);
+                layers[1][0, z] = Math.Max(Math.Min((average + sign * randy.Next(0, distMult + 1)), maxValue), minValue);
+            }
+        }
+
+        return layers;
+    }
+
+
     // Private supporting methods
+    private static int[,] CreateStandardIntLayer(int WORLDX, int WORLDZ, int minValue, int maxValue, int distMult)
+    {
+        int[,] layer = new int[WORLDX, WORLDZ];
+        System.Random randy = new System.Random();
+        int sign;
+        int average;
+
+        // First do the upper left square
+        layer[0, 0] = randy.Next(minValue, maxValue);
+        // Then do the top row based on the square to its left
+        for (int x = 1; x < WORLDX; x++)
+        {
+            sign = GenerateRandomSign(randy);
+            layer[x, 0] = Math.Max(Math.Min((layer[x - 1, 0] + sign * randy.Next(0, distMult + 1)), maxValue), minValue);
+        }
+        // Then do the left column based on the square above it
+        for (int z = 1; z < WORLDZ; z++)
+        {
+            sign = GenerateRandomSign(randy);
+            layer[0, z] = Math.Max(Math.Min((layer[z - 1, 0] + sign * randy.Next(0, distMult + 1)), maxValue), minValue);
+        }
+        // Then do every other cell in order
+        for (int z = 1; z < WORLDX; z++)
+        {
+            for (int x = 1; x < WORLDX; x++)
+            {
+                sign = GenerateRandomSign(randy);
+                average = (int) Math.Round((layer[z - 1, x] + layer[z - 1, x - 1] + layer[z, x - 1]) / 3.0, 0);
+                layer[0, z] = Math.Max(Math.Min((average + sign * randy.Next(0, distMult + 1)), maxValue), minValue);
+            }
+        }
+
+        return layer;
+    }
+    
+    // Generate a random negative sign
     private static int GenerateRandomSign(System.Random randy)
     {
         int num = randy.Next(0, 2);
