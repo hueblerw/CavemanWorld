1b7c93a4f335cafdeebf4eb26eeb4cb14ea86055
William Huebler <hueblerw@gmail.com>
2016-11-02 10:36:11 -0500

    Create a new class to generate the basic files

---
+++ DynamicCavemanWorld/Assets/Classes/DataGenerator.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DataGenerator {
+
+    // constructor
+    public DataGenerator()
+    {
+    }
+
+    // Static methods for creating layers based on inputs:
+    // ***************************************************
+    // Standard Layer - float [,]
+    public static float[,] CreateStandardFloatLayer(int WORLDX, int WORLDZ, double minValue, double maxValue, double distMult)
+    {
+        float[,] layer = new float[WORLDX, WORLDZ];
+        Random randy = new Random();
+
+        // First do the upper left square
+        layer[0, 0] = randy.NextDouble(minValue, maxValue);
+    }
+
+}
