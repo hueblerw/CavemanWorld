0b753056d9eb4b17317b692a5ac9f1be88088e52
William Huebler <hueblerw@gmail.com>
2017-01-26 10:47:58 -0600

    Create a script for keyboard controller of the camera object in the x and z planes

---
+++ DynamicCavemanWorld/Assets/Controllers/CameraController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    // Update is called once per frame
+    public float speed = 1.0f;
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.position = new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position = new Vector3(speed * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position = new Vector3(0, 0, speed * Time.deltaTime,);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position = new Vector3(0, 0, speed * Time.deltaTime);
+        }
+    }
+}
