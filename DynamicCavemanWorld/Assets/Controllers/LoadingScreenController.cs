ea3389516917d87413903a0017d3708456e361fb
William Huebler <hueblerw@gmail.com>
2017-02-23 12:57:42 -0600

    Save the project

--- DynamicCavemanWorld/Assets/Controllers/LoadingScreenController.cs
+++ DynamicCavemanWorld/Assets/Controllers/LoadingScreenController.cs
@@ -16,7 +16,6 @@ public class LoadingScreenController : MonoBehaviour {
     // Use this for initialization
     void Start () {
         
-
         if (TheWorld == null)
         {
             Debug.Log("Load World");
